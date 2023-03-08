d1e643a21b8853fa2b5c14a9dec8e1b6bb5d2e46
Muntaha Tahiat <thundertahi@gmail.com>
2023-03-08 19:48:54 +0600

    Added

--- linkedlist.py
+++ linkedlist.py
@@ -43,7 +43,6 @@ change the previous value to 5, and current to 3 and change what its facing None
 repeat the process, until the current value is null
 then the previous value at the end of the loop should be our NEW HEAD
 
-test
 
 
 '''
@@ -69,3 +68,183 @@ class Solution(object):
         return prev
     
 
+'''
+
+Linked List
+
+EASY
+2. Merge Two Sorted Lists:
+
+# QUESTION
+
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+
+
+Example 1:
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+
+# SOLUTION 
+
+iterative:
+example: 
+list1 = 1 -> 3 -> 4
+list2 = 3 -> 5 -> 6
+
+Create a dummy node as a placeholder for the head of the merged list, and set tail to point to it:
+dummy -> None
+         ^
+       tail
+
+In the first iteration of the loop, compare the values of the heads of list1 and list2, which are 1 and 2, respectively. Since 1 is smaller, set tail.next to the node with value 1, and update tail to point to that node:
+dummy -> 1 -> None
+              ^
+            tail
+
+In the second iteration of the loop, compare the values of the heads of list1 and list2, which are now 3 and 2, respectively. Since 2 is smaller, set tail.next to the node with value 2, and update tail to point to that node:
+dummy -> 1 -> 2 -> None
+                   ^
+                 tail
+
+In the third iteration of the loop, compare the values of the heads of list1 and list2, which are now 3 and 5, respectively. Since 3 is smaller, set tail.next to the node with value 3, and update tail to point to that node:
+dummy -> 1 -> 2 -> 3 -> None
+                        ^
+                      tail
+
+etc. etc 
+
+in the case where one list might be longer than the other like =>
+list1 = 1 -> 3 -> 4 
+list2 = 3 -> 5 -> 6 -> 8 -> 9
+
+bcz its previously sorted, we just need to add it to the list. no further rearrangement requirent
+
+loop ends at: 1 -> 3 -> 3 -> 4 -> None
+
+take the remainder of that list:  5 -> 6 -> 8 -> 9
+and assign tail.next to it =  5 -> 6 -> 8 -> 9
+
+
+'''
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode() #dummy node
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else: #if equivalent or l2 is smaller
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        # if after the loop any list has remaining nodes
+
+        if list1:
+            tail.next = list1 
+
+        if list2:
+            tail.next = list2 
+
+        return dummy.next #head
+    
+
+
+'''
+Linked List
+
+EASY
+1. Linked List Cycle:
+
+# QUESTION
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+# SOLUTION
+
+1. hashset: T - O(n) M - O(n) cus using hashmaps
+add a node to a hashset after we visit
+so if it is a cycle, we will be alerted if a node gets repeated 
+
+2. two pointers + floyds tortoise and hare: T - O(n) M - O(1)
+
+if it is a cycle, 
+no matter where the two points are, 
+if theyre moving at diff speeds, they'll end up catching up to each other 
+
+create two points slow and fast. slow moves one step, fast moves two. and give them the same starting point
+start a while loop according to fast, cus if not cycle, fast will get a null val first
+if fast and slow nodes are ever the same its a cycle, else false
+
+
+
+
+'''
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
---
+++ trees.py
@@ -0,0 +1,103 @@
+'''
+Trees
+
+EASY
+1. Invert Binary Tree:
+
+# QUESTION
+Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+Example 1:
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+
+Input: root = []
+Output: []
+
+# SOLUTION
+
+1. recursive  DFS -Depth First Search recursive algorithm to search all the vertices of a tree data structure
+
+        4                                          
+      /   \
+     7     2
+    / \   / \
+   9   6 3   1
+
+    --inverse--
+
+        4
+      /   \
+     2     7
+    / \   / \
+   1   3 6   9
+
+during the inverse: the left and right subtrees of each node are swapped
+The function invertTree() then recursively swaps the left and right subtrees of each node starting from the root
+because it calls itself in the function
+
+
+'''
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root: #no more nodes in the tree to traverse and modify 
+            return None
+        
+        tmp = root.right #2
+        root.right = root.left # 2 -> 7
+        root.left = tmp # 7 -> 2
+
+        self.invertTree(root.right) # passed as the root parameter to recursively inverse children
+        self.invertTree(root.left) ## passed as the root parameter to recursively inverse children
+
+        return root
+    
+
+    
+'''
+Trees
+
+EASY
+2. Maximum Depth of Binary Tree:
+
+# QUESTION
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+
+# SOLUTION
+1. recusrive dfs:
+
+
+ 
+
+'''
+
